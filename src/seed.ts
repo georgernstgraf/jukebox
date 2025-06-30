c86a471f4276f576556547f242a8aad3e60d4e9e
tom <tom@spg.at>
2025-06-30 16:59:31 +0200

    tsc works again

--- src/seed.ts
+++ src/seed.ts
@@ -1,8 +1,10 @@
 import { createReadStream, writeFileSync } from "fs";
 import { createInterface, Interface } from "readline";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
+const BATCHSIZE = 1000;
 /**
- * Reads a file line by line asynchronously using streams.
  * @param {string} filePath The path to the file to read.
  * @returns {Promise<void>} A promise that resolves when the file has been fully read.
  */
@@ -34,6 +36,33 @@ async function readLinesFromFile(filePath: string): Promise<void> {
     });
 }
 
+async function* insert(track: string): AsyncGenerator<void> {
+    const newTracks = new Set<string>();
+    while (track) {
+        newTracks.add(track);
+        if (newTracks.size >= BATCHSIZE) {
+            console.log(`Inserting batch of ${newTracks.size} tracks...`);
+            await createTracksBatch(newTracks);
+            newTracks.clear();
+        }
+        yield;
+    }
+    if (newTracks.size > 0) {
+        console.log(`Inserting final batch of ${newTracks.size} tracks...`);
+        await createTracksBatch(newTracks);
+    }
+    return;
+}
+async function createTracksBatch(tracks: Set<string>): Promise<void> {
+    const existing = await prisma.track.findMany({
+        where: {
+            path: {
+                in: Array.from(tracks),
+            },
+        },
+    });
+    console.log(`Simulating insertion of ${tracks.size} tracks...`);
+}
 const myFilePath: string = process.argv[2];
 console.log(`Starting to read file: ${myFilePath}\n`);
 readLinesFromFile(myFilePath)
