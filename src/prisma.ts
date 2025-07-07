063a8855cfcd41ddc4fe32ca1c8aba249401e2c8
Georg Graf (SPG) <grafg@spengergasse.at>
2025-07-07 09:06:00 +0200

    increase timeout

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,2 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 export const prisma = new PrismaClient();
+let timeout = Number.parseInt(
+    process.env.DB_TIMEOUT ? process.env.DB_TIMEOUT : "21000",
+);
+try {
+    await prisma.$queryRawUnsafe(`PRAGMA busy_timeout = ${timeout};`);
+    console.log(`SQLite busy_timeout set to ${timeout}ms`);
+} catch (error) {
+    console.error("Error setting busy_timeout:", error);
+}
