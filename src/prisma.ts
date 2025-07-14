40917afbc88bd64c60276025b5cc747b2543cf2a
Georg Graf (SPG) <grafg@spengergasse.at>
2025-07-14 11:08:58 +0200

    computed attribute

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,9 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 import { config } from "./env.js";
-export const prisma = new PrismaClient();
+const prisma = new PrismaClient().$extends({
+    name: 'hasId3-extension',
+    result: {
+        track: {
+            hasId3: {
+                needs: { artist: true, album: true, title: true },
+                compute(track) {
+                    return !!(track.artist && track.album && track.title);
+                },
+            },
+        },
+    },
+});
 try {
     const result = await prisma.$queryRawUnsafe(`PRAGMA busy_timeout = ${config.dbTimeout};`);
     console.log(`SQLite busy_timeout set to ${config.dbTimeout}ms. (${result})`);
 } catch (error) {
     console.error("Error setting busy_timeout:", error);
 }
+export { prisma };
